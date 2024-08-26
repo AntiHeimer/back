472e2507b223a7611fe779ab4138f27c00a30fd3
ziyoungkwon <gy011003@naver.com>
2024-08-26 12:47:28 +0900

    sleepdata 저장 리포지토리 작성중

--- src/main/java/capstone/Antiheimer/controller/SleepDataController.java
+++ src/main/java/capstone/Antiheimer/controller/SleepDataController.java
@@ -1,4 +1,36 @@
 package capstone.Antiheimer.controller;
 
+import capstone.Antiheimer.dto.NormalResDto;
+import capstone.Antiheimer.dto.SaveMoveReqDto;
+import capstone.Antiheimer.dto.SaveSleepReqDto;
+import capstone.Antiheimer.service.SleepDataService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
 public class SleepDataController {
+
+    @Autowired
+    private final SleepDataService sleepDataService;
+
+    /**
+     * 수면데이터 저장
+     * @param request
+     * @return NormalResDto
+     */
+    @PostMapping("/save/sleep")
+    public NormalResDto saveSleep(@RequestBody SaveSleepReqDto request) {
+
+        try {
+
+            log.info("Sleep데이터 저장 시작");
+            sleepDataService.insertSleep(request);
+        }
+    }
 }
--- src/main/java/capstone/Antiheimer/repository/SleepDataRepository.java
+++ src/main/java/capstone/Antiheimer/repository/SleepDataRepository.java
@@ -1,4 +1,24 @@
 package capstone.Antiheimer.repository;
 
+import capstone.Antiheimer.domain.HealthData;
+import capstone.Antiheimer.dto.SaveSleepReqDto;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.parser.Entity;
+
+@Repository
+@RequiredArgsConstructor
 public class SleepDataRepository {
+
+    private final EntityManager em;
+    private final MemberRepository memberRepository;
+
+    public void saveSleep (SaveSleepReqDto request) {
+
+        HealthData healthData = new HealthData();
+
+
+    }
 }
