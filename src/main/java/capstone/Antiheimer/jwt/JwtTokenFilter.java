f3ff307a42543de22d37d7a82ab1f9520f76e513
ziyoungkwon <gy011003@naver.com>
2024-09-01 03:48:59 +0900

    JwtTokenFilter 수정(location 추가)

--- src/main/java/capstone/Antiheimer/jwt/JwtTokenFilter.java
+++ src/main/java/capstone/Antiheimer/jwt/JwtTokenFilter.java
@@ -35,7 +35,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
             System.out.println("requestURI = " + requestURI);
 
             // 회원가입 경로에 대해서는 필터링을 건너뜁니다.
-            if ("/signup".equals(requestURI) || "/login".equals(requestURI) || "/".equals(requestURI)) {
+            if ("/signup".equals(requestURI) || "/login".equals(requestURI) || "/".equals(requestURI) || "/save/location".equals(requestURI)) {
                 filterChain.doFilter(request, response);
                 return;
             }
