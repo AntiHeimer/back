2eba42747790a6aa5df79728b19715207a16725b
yereumi <lovelyyeji01@gmail.com>
2024-09-03 13:07:16 +0900

    git 헤메기, build

---
+++ src/main/java/capstone/Antiheimer/controller/NotificationController.java
@@ -0,0 +1,21 @@
+package capstone.Antiheimer.controller;
+
+import capstone.Antiheimer.dto.NotificationReq;
+import capstone.Antiheimer.service.FcmService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private final FcmService fcmService;
+
+    @PostMapping("/send-notification")
+    public String sendNotification(@RequestBody NotificationReq request) {
+        fcmService.sendNotification(request.getTargetToken(), request.getTitle(), request.getBody());
+        return "Notification sent!";
+    }
+}
