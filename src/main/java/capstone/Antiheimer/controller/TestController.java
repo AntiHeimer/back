3f7768ca7514c5bf2afbcaed83e532b1cb90321e
yereumi <lovelyyeji01@gmail.com>
2024-09-03 15:47:25 +0900

    jar 파일 오류, build

--- src/main/java/capstone/Antiheimer/controller/TestController.java
+++ src/main/java/capstone/Antiheimer/controller/TestController.java
@@ -12,4 +12,9 @@ public class TestController {
     public ResponseEntity<String> HelloWorld() {
         return new ResponseEntity<>("Hello World", HttpStatus.OK);
     }
+
+    @GetMapping("/check-token")
+    public ResponseEntity<String> checkToken() {
+        return new ResponseEntity<>("토큰 확인 완료", HttpStatus.OK);
+    }
 }
