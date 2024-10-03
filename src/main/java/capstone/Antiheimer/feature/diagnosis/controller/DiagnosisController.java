5a5e91c39829694463fdbc0aa6049b37734fc2c6
ziyoungkwon <gy011003@naver.com>
2024-10-04 04:25:14 +0900

    aes 복호화 없앰

--- src/main/java/capstone/Antiheimer/feature/diagnosis/controller/DiagnosisController.java
+++ src/main/java/capstone/Antiheimer/feature/diagnosis/controller/DiagnosisController.java
@@ -136,15 +136,11 @@ public class DiagnosisController {
     @PostMapping("/finish")
     public ResponseEntity<TestResDto> diagnosisResult(@RequestBody DiagnosisResultReqDto request) throws UnsupportedEncodingException, JsonProcessingException{
 
-        log.info("[Controller] 멤버 UUID AES 복호화");
-        String decryptedRequest = URLDecoder.decode(request.getMemberUuid(), StandardCharsets.UTF_8.name());
-        DiagnosisResultReqDto reqDto = objectMapper.readValue(decryptedRequest, DiagnosisResultReqDto.class);
+        log.info(("[Controller] 결과 저장 및 치매 진단 시작"));
+//        AiResDto aiResDto = diagnosisService.getDiagnosisResult(request);
+        Diagnosis diagnosis = diagnosisService.getDiagnosisResult(request);
 
-        log.info(("[Controller] 답안 채점 및 점수 계산 시작"));
-//        AiResDto aiResDto = diagnosisService.getDiagnosisResult(reqDto);
-        Diagnosis diagnosis = diagnosisService.getDiagnosisResult(reqDto);
-
-        log.info(("[Controller] 답안 채점 및 점수 계산 성공"));
+        log.info(("[Controller] 결과 저장 및 치매 진단 성공"));
 //        return new ResponseEntity<>(new DiagnosisResultResDto("200", "진단 결과 저장 및 치매진단 성공", aiResDto), HttpStatus.OK);
         return new ResponseEntity<>(new TestResDto("200", "점수계산성공ㅇ", diagnosis), HttpStatus.OK);
     }
