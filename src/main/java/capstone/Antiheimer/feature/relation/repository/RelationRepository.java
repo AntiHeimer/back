f903d392d75d0b79abe672c4070389a5f3d54be7
yereumi <lovelyyeji01@gmail.com>
2024-11-02 16:01:44 +0900

    피보호자 보호자 조회 수정

--- src/main/java/capstone/Antiheimer/feature/relation/repository/RelationRepository.java
+++ src/main/java/capstone/Antiheimer/feature/relation/repository/RelationRepository.java
@@ -80,7 +80,7 @@ public class RelationRepository {
      */
     public List<InfoGuardianDto> infoGuardian(String memberUuid) {
 
-        return em.createQuery("SELECT new capstone.Antiheimer.feature.relation.dto.info.InfoGuardianDto(m.uuid, m.id, m.name) FROM Member m JOIN Relation r ON m.uuid = r.guardianUuid WHERE r.wardUuid = :memberUuid", InfoGuardianDto.class)
+        return em.createQuery("SELECT new capstone.Antiheimer.feature.relation.dto.info.InfoGuardianDto(m.uuid, m.id, m.name) FROM Member m JOIN Relation r ON m.uuid = r.guardianUuid WHERE r.wardUuid = :memberUuid AND r.active = true", InfoGuardianDto.class)
                 .setParameter("memberUuid", memberUuid)
                 .getResultList();
     }
@@ -92,7 +92,7 @@ public class RelationRepository {
      */
     public List<InfoWardDto> infoWard(String memberUuid) {
 
-        return em.createQuery("SELECT new capstone.Antiheimer.feature.relation.dto.info.InfoWardDto(m.uuid, m.id, m.name) FROM Member m JOIN Relation r ON m.uuid = r.wardUuid WHERE r.guardianUuid = :memberUuid", InfoWardDto.class)
+        return em.createQuery("SELECT new capstone.Antiheimer.feature.relation.dto.info.InfoWardDto(m.uuid, m.id, m.name) FROM Member m JOIN Relation r ON m.uuid = r.wardUuid WHERE r.guardianUuid = :memberUuid AND r.active = true", InfoWardDto.class)
                 .setParameter("memberUuid", memberUuid)
                 .getResultList();
     }
